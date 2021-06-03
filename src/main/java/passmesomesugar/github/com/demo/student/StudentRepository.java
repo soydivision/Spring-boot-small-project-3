eb8ee2d395dedebb394a42ab1fb008ee2395f228
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2021-06-03 15:55:52 +0600

    Works. Postgre connected.

---
+++ src/main/java/passmesomesugar/github/com/demo/student/StudentRepository.java
@@ -0,0 +1,8 @@
+package passmesomesugar.github.com.demo.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Long> {
+}
